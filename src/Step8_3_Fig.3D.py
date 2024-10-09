1171993a586d80daa1603e5b88a6d9c34235df2f
xiaoming047 <luxforry047@gmail.com>
2024-10-09 16:00:55 -0400

    Update Step8_3_Fig.3D.py

--- src/Step8_3_Fig.3D.py
+++ src/Step8_3_Fig.3D.py
@@ -10,7 +10,9 @@ from matplotlib.ticker import FuncFormatter
 from my_module import scientific_formatter
 
 plt.rcParams['font.family'] = 'DejaVu Sans'
-project_root = os.path.dirname(os.path.abspath(__file__))
+
+current_path = os.path.dirname(os.path.abspath(__file__))
+project_root = os.path.dirname(current_path)
 result_path = os.path.join(project_root, 'output/')
 fig_path = os.path.join(project_root, 'paper_fig/')
 
