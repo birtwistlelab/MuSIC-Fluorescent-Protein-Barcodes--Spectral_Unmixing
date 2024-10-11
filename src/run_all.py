a945ba2a44da20b7fd5cb9dd91e3d7519cd6622d
xiaoming047 <luxforry047@gmail.com>
2024-10-11 13:17:06 -0400

    Update run_all.py

--- src/run_all.py
+++ src/run_all.py
@@ -1,28 +1,30 @@
 import traceback
+import importlib.util
+import os
 
 step_files = [
-    "my_module.py",
-    "Step1_DataExtractionForReference(pos_pR_fp).py",
-    "Step2_Profile_Reference.py",
-    "Step3_DataExtractionForProbeUnmixing(pR_fp_singlets).py",
-    "Step4_EntirePositivePopulationGating(FI_vs_frequency_hist).py",
-    "Step5_UnmixPositiveCells(pR_fp_singlets).py",
-    "Step6_Create18ROCCurves.py",
-    "Step7_DetermineTheOptimal_ThresholdsForEachProbe.py",
-    "Step8_1_Condition1_Original.py",
-    "Step8_2_Identify_Each_Cell.py",
-    "Step8_3_Fig.3D.py",
-    "Step9.Condition2_Intensity_Cutoff.py",
-    "Step10_1_Condition3_Remove_mTFP1.py",
-    "Step10_2_determine_the_threshold_without_mTFP1.py",
-    "Step10_3_FPR.py",
-    "Step11_Condition4_IntensityCutoff_mTFP1Removal.py",
-    "Step12_FPR_summary_of_4_conditions.py",
-    "Step13_DataExtractionForUnmixing(pos_pMuSICs).py",
-    "Step14_pMuSICs_condition4_IntensityCutoff_mTFP1Removal.py",
-    "Step15_pMuSICs_condition4_Calculate_TPR.py",
-    "Step16_Trouble_shooting_for_fig.S3.py",
-    "Step17_Reference_Spectrum_for_fig.S2.py"
+    "my_module",
+    "Step1_DataExtractionForReference_pos_pR_fp",
+    "Step2_Profile_Reference",
+    "Step3_DataExtractionForProbeUnmixing_pR_fp_singlets",
+    "Step4_EntirePositivePopulationGating_FI_vs_frequency_hist",
+    "Step5_UnmixPositiveCells_pR_fp_singlets",
+    "Step6_Create18ROCCurves",
+    "Step7_DetermineTheOptimal_ThresholdsForEachProbe",
+    "Step8_1_Condition1_Original",
+    "Step8_2_Identify_Each_Cell",
+    "Step8_3_Fig_3D",
+    "Step9_Condition2_Intensity_Cutoff",
+    "Step10_1_Condition3_Remove_mTFP1",
+    "Step10_2_determine_the_threshold_without_mTFP1",
+    "Step10_3_FPR",
+    "Step11_Condition4_IntensityCutoff_mTFP1Removal",
+    "Step12_FPR_summary_of_4_conditions",
+    "Step13_DataExtractionForUnmixing_pos_pMuSICs",
+    "Step14_pMuSICs_condition4_IntensityCutoff_mTFP1Removal",
+    "Step15_pMuSICs_condition4_Calculate_TPR",
+    "Step16_Trouble_shooting_for_fig_S3",
+    "Step17_Reference_Spectrum_for_fig_S2"
 ]
 
 
@@ -30,8 +32,8 @@ def run_all_steps():
     for step in step_files:
         try:
             print(f"Running {step}...")
-            with open(step) as file:
-                exec(file.read())
+            module = importlib.import_module(step)
+
             print(f"{step} completed.\n")
         except Exception as e:
             print(f"Error while running {step}: {e}")
